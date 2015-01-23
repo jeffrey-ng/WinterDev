9126d8ed10461ed039ee592eb093ede76c85f804
Jeffrey Ng <jeffrey.jn.ng@gmail.com>
2015-01-23 16:58:33 -0500

    Init comp322

---
+++ comp322/a1/Makefile
@@ -0,0 +1,10 @@
+all: testytest
+
+testytest:
+	$ g++ -std=c++11 -o testytest continuedTest.cpp continuedStudent.cpp continued.h
+
+gradygrade:
+	$ g++ -std=c++11 -o gradygrade continuedGrading.cpp continuedStudent.cpp continued.h
+
+clean: 
+	rm -rf *.o testytest gradygrade
---
+++ comp322/a1/continuedStudent.cpp
@@ -0,0 +1,27 @@
+#include "continued.h"
+
+unsigned int gcd(unsigned int a, unsigned int b){
+
+    if (a==0) {
+        return b;
+    }
+    return gcd(b%a, a);
+}
+
+ContinuedFraction *getCFlargerThanOne(unsigned int b, unsigned int a) {
+  // your code here
+}
+
+ContinuedFraction *getCF(unsigned int b, unsigned int a) {
+  // your code here
+}
+
+
+ContinuedFraction *getCF(unsigned int head, ContinuedFraction *fixed, ContinuedFraction *period) {
+  // your code here
+}
+
+
+Fraction getApproximation(ContinuedFraction *fr, unsigned int n) {
+  // your code here
+}
