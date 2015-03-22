11b871b085b00f2b2bf5e3150232450fd7ac7df2
Jeffrey Ng <jeffrey.jn.ng@gmail.com>
2015-03-22 16:17:14 -0400

    Done a3

--- comp322/a3/gardensStudent.cpp
+++ comp322/a3/gardensStudent.cpp
@@ -1,3 +1,4 @@
+
 #include "gardens.h"
 #include <stack>
 #include <iostream>
@@ -10,13 +11,13 @@ Fraction ContinuedFraction::getApproximation(unsigned int k) const {
   vector<cf_int> fixedPart;
   // we go backwards from the end to the beginning to compute the approximation
   // z is the index that keeps track of the position reached - starts at n-1
-  int z = k-1;
+  int z = k;
 
   // start with 1/0
   toRet.numerator = 1;
   toRet.denominator = 0;
 
-  while(!hasNoMore() || fixedPart.size() < z) {
+  while(!hasNoMore() && fixedPart.size() < z) {
    fixedPart.push_back(next());
 
   }
@@ -132,8 +133,6 @@ bool MagicBoxCF::hasNoMore() const {
     cf_int *m_j = (mbnums+1);
     cf_int *m_k = (mbnums+2);
     cf_int *m_l = (mbnums+3);
-
-
     
     while( ((*m_k == 0 || *m_l == 0) && !(*m_k == 0 && *m_l == 0)) ||
           (*m_k != 0 && *m_l != 0 && *m_i / *m_k != *m_j / *m_l) ) {
@@ -150,10 +149,6 @@ bool MagicBoxCF::hasNoMore() const {
         //read p
         cf_int p = boxedFraction->next();
         
-//        (*mbnums)++;
-//        mbnums = mbnums + 1;
-//        auto xxx = mbnums[1];
-        
         //change box accordingly
         cf_int i = *m_j,
         j = *m_i + *m_j * p,
@@ -167,35 +162,6 @@ bool MagicBoxCF::hasNoMore() const {
     if(*m_k == 0 && *m_l == 0) return true;
     
     return false;
-//    return q;
-    
-//    while(true){
-////        int i = (box).i; int j = (box).j;
-////        int k = (box).k; int l = (box).l;
-//        cf_int i = *mbnums;
-//        cf_int j = *(mbnums+1);
-//        cf_int k = *(mbnums+2);
-//        cf_int l = *(mbnums+3);
-//        
-//        if (k==0 && l==0)
-//            return true; //reached end
-//        
-//        else if (k==0 || l==0 || (i/k != j/l)) {
-////            int p = spit((box).boxedFraction, (box).indexInBoxedFraction++);
-//            cf_int p = boxedFraction->next();
-//            if(p!=-1) {
-//                (box).i =j; (box).j =i+j*p;
-//                (box).k =l; (box).l =k+l*p;
-//            }
-//            else {
-//                (box).i =j; (box).j =j;
-//                (box).k =l; (box).l =l; }}
-//        else {
-//            int q = i/k;
-//            (box).i =k; (box).j =l;
-//            (box).k =i-k*q; (box).l =j-l*q;
-//            return q; }}
-
 }
 
 void MagicBoxCF::resetIterator() const {
@@ -203,6 +169,7 @@ void MagicBoxCF::resetIterator() const {
     mbnums[1] = b;
     mbnums[2] = 1;
     mbnums[3] = 0;
+    
     boxedFraction->resetIterator();
 
 }
@@ -253,24 +220,45 @@ ostream &operator<<(ostream& outs, const ContinuedFraction &cf) {
 /* QUESTION 6 */
 
 float Flower::getAngle(unsigned int k) const {
-    //TODO
- return 0;
+   //TODO
+
+    Fraction fr = theta->getApproximation(apx_length);
+    // compute fractional part of the rotations
+    double fractpart = ((k*fr.numerator)%fr.denominator/(double)fr.denominator);
+    //return
+    return (2 * M_PI * fractpart);
+    
 }
 
 //TODO : Flower::Flower(const ContinuedFraction *f, unsigned int apxLengthParam) ...
 
 
-Flower::Flower(const ContinuedFraction *f, unsigned int apxLengthParam): apx_length{0} {
-
+Flower::Flower(const ContinuedFraction *f, unsigned int apxLengthParam): apx_length{apxLengthParam} {
+    this->theta = f;
 }
 
-//Seed Flower::getSeed(unsigned int k) const {
-//    //TODO
-//}
+Seed Flower::getSeed(unsigned int k) const {
+    Seed s;
+    // we follow the formulas in the assignment's document
+    float angle = getAngle(k);
+    
+    float radius = sqrt(k/(pie*1.0));
+//    if (radius < 1) {
+//        radius = 1;
+//    }
+    
+    s.x = radius * cos(angle);
+    s.y = radius * sin(angle);
+    return s;
+}
 
-//vector<Seed> Flower::getSeeds(unsigned int k) const {
-//    //TODO
-//}
+vector<Seed> Flower::getSeeds(unsigned int k) const {
+    vector<Seed> toRet;
+    for (int i = 0; i< k; i++) {
+        toRet.push_back(getSeed(i));
+    }
+    return toRet;
+}
 
 
 Flower::~Flower() {
@@ -281,6 +269,24 @@ Flower::~Flower() {
 
 void Flower::writeMVGPicture(ostream &out, unsigned int k, unsigned int scale_x, unsigned int scale_y) const {
     //TODO
+    //get every seed of flower
+    vector<Seed> mySeeds = getSeeds(k);
+    int C_x;
+    int C_y;
+    int B_x;
+    int B_y;
+    for (int i = 0; i < mySeeds.size();i++){
+        Seed s = mySeeds.at(i);
+        C_x = (scale_x/2) + s.x * ((scale_x-200)/2) * sqrt(pie/k);
+        C_y = (scale_y/2) + s.y * ((scale_y-200)/2) * sqrt(pie/k);
+        
+        
+        B_x = C_x + sqrt((float)i/k) * (min(scale_x,scale_y)/100);
+        B_y = C_y;
+        
+        out << "fill blue circle "<<C_x<<","<<C_y<<" "<<B_x<<","<<B_y << endl;
+    }
+    
 }
 
 
